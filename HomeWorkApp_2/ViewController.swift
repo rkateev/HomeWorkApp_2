c54ed843020ebb680589fb1207877fbf4b5d7edb
rkateev <60319058+rkateev@users.noreply.github.com>
2021-07-08 00:56:31 +0300

    added interface elements

--- HomeWorkApp_2/ViewController.swift
+++ HomeWorkApp_2/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
